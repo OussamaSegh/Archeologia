6c0591160726c495d1eb39f0242375cea451b597
Seghaier Oussama <oussama.seghaier@gmail.com>
2018-04-08 16:59:34 +0200

    init

---
+++ help.py
@@ -0,0 +1,5 @@
+#python3 utf-8
+#@autor = Seghaier Oussama
+#this function displays the help window
+
+
---
+++ home.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on sun Apr  8 15:11:51 2018
+
+@author: oseghaier
+
+@description = this function displays the home tkinter window 
+
+
+"""
+
